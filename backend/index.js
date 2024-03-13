d7aacb076e75d0b8beee88cf32364ea855dd1d80
ANANTH-SWAMY <ananth.gnr@gmail.com>
2024-03-13 11:55:59 +0530

    Added ssl certificate

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 const express = require("express")
 const { Server } = require("socket.io")
 const http = require("http")
+const https = require("https")
+const fs = require("fs")
 const cors = require("cors")
 
 const app = express()
 app.use(cors())
 
-const server = http.createServer(app)
+const options = {
+	key: fs.readFileSync("./ssl/private_key.key"),
+	cert: fs.readFileSync("./ssl/certificate.crt")
+}
+
+const server = http.createServer(options,app)
 const io = new Server(server,{
 	cors: {
 		origin: "http://localhost:5173",
