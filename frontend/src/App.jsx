d7abb5497eca858d0bb1d53fa9085d42264dd948
Persay1981 <75056681+Persay1981@users.noreply.github.com>
2024-03-25 20:10:34 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,21 +9,24 @@ import Header from "./components/Header"
 function App() {
 	const [username, setUsername] = useState("")
 	const [messages, setMessages] = useState([])
+	const [groupChatName,setGroupChat] = useState("")
 
 	useEffect(() => {
-		socket.off("receieve_message").on("receieve_message", (message) => {
+		socket.off("receive_message").on("receive_message", (message) => {
 			setMessages(messages => [...messages,message])
 		})
 	}, [socket])
 
+	//console.log(messages)
+
 	return(
 		<>
-			{!username && <Login setUsername={setUsername}/>}
+			{!username && <Login setUsername={setUsername} setGroupChat={setGroupChat}/>}
 			{username && 
 				<div className="h-svh flex flex-col items-center justify-between">
-					<Header username={username} setUsername={setUsername} setMessages={setMessages}/>
+					<Header username={username} setUsername={setUsername} setMessages={setMessages} groupChat={groupChatName}/>
 					<ChatBody messages={messages} className="grow"/>
-					<ChatInput uname={username} setMessage={setMessages} messages={messages} className="bottom-0"/>
+					<ChatInput uname={username} setMessage={setMessages} messages={messages} groupChat={groupChatName} className="bottom-0"/>
 				</div>
 			}
 		</>
