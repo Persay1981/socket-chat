23c870a32b886863e76c366a0c2691755e010b9b
Persay1981 <75056681+Persay1981@users.noreply.github.com>
2024-03-25 20:09:55 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,18 +1,30 @@
 import {useState} from "react"
+import { socket } from "../socket"
 
-function Login({setUsername}){
+function Login({setUsername,setGroupChat}){
 	const [textInput, setTextInput] = useState("")
+	const [groupInput, setGroupInput] = useState("")
 
 	const handleSubmit = () => {
 		textInput && setUsername(textInput)
+		if(groupInput){
+			setGroupChat(groupInput)
+			socket.emit('join',groupInput)
+		}
 	}
 
 	return(
 		<div className="flex items-center justify-center h-svh">
-			<div className="p-4 flex sm:flex-row flex-col items-center justify-center gap-4 bg-base-300 rounded-lg">
+			<div className="p-4 sm:p-6 flex items-center justify-center gap-4 bg-base-300 rounded-lg">
 				<input
 					placeholder="Name..."
 					onChange={(e) => {setTextInput(e.target.value)}}
+					className="input input-bordered bg-base-300 focus:outline-none"
+					autoFocus
+				></input>
+				<input
+					placeholder="Group Name..."
+					onChange={(e) => {setGroupInput(e.target.value)}}
 					className="input input-bordered bg-base-300 focus:outline-none"
 					onKeyDown={(e) => {e.key === "Enter" && handleSubmit()}}
 					autoFocus
